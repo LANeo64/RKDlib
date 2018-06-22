c44c49294c80adf3465fee5560da1f86c82d524f
Petr Grenar <grenar.petr@gmail.com>
2018-06-22 21:55:05 +0200

    - Doplnene metody pro get/set sekce a add/rem souprava - chybi jeste rem souprava podle jmena

--- Code/BlokStav.cpp
+++ Code/BlokStav.cpp
@@ -19,11 +19,11 @@ BlokStav::BlokStav(bool stav, bool napajeni, bool zkrat, bool dcc, int sekce) {
 }
 
 void BlokStav::SetSekce(int key, bool val){
-
+    m_sekce.insert(m_sekce.begin() + key, val);
 }
 
 bool BlokStav::GetSekce(int key){
-
+    return m_sekce[key];
 }
 
 void BlokStav::SetStav(bool val){
@@ -35,13 +35,13 @@ bool BlokStav::GetStav(){
 }
 
 void BlokStav::AddSouprava(string souprava){
-
+    m_soupravy.insert(m_soupravy.begin(), souprava);
 }
 
 void BlokStav::RemSouprava(int key){
-
+    m_soupravy.erase(m_soupravy.begin() + key);
 }
 
 void BlokStav::RemSouprava(string name) {
-
+    //zatim neni hotova trida pro soupravu...
 }
